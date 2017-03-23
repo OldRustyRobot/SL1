87410e92268e22b52aec4bc72b6ae132175ad91d
Ketamineface <50thcaliber@gmail.com>
2017-03-23 21:45:33 +0200

    commit 2

---
+++ SL2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SL2
+//
+//  Created by 8thball on 23.03.17.
+//  Copyright (c) 2017 8thball. All rights reserved.
+//
+
+import Foundation
+
+let foo = "hello world"
+print(foo)
